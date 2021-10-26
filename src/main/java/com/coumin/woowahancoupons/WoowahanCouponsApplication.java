adbb374367fd3f6cd977ed375328da05881e69ef
LeeJuOh <>
2021-10-26 14:59:20 +0900

    [WOOCOU-18] 🎉 spring boot init

---
+++ src/main/java/com/coumin/woowahancoupons/WoowahanCouponsApplication.java
@@ -0,0 +1,13 @@
+package com.coumin.woowahancoupons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowahanCouponsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoowahanCouponsApplication.class, args);
+    }
+
+}
