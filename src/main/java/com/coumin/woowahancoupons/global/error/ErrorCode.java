c8b0186fa661f1cdaad9310d86fc59fd224d5547
LeeJuOh <>
2021-10-29 20:57:18 +0900

    ⭐ feat : ErrorCode 추가 및 exception 리팩토링

--- src/main/java/com/coumin/woowahancoupons/global/error/ErrorCode.java
+++ src/main/java/com/coumin/woowahancoupons/global/error/ErrorCode.java
@@ -1,11 +1,17 @@
 package com.coumin.woowahancoupons.global.error;
 
+import lombok.Getter;
+
+@Getter
 public enum ErrorCode {
 
     UNEXPECTED(-1, "-1", "Unexpected exception occurred"),
+
     ENTITY_NOT_FOUND(400, "C001", " Entity Not Found"),
     COUPON_WALLET_NOT_FOUND(400, "C002", "Can not find a CouponWallet for id "),
-    STORE_NOT_FOUND(400, "C003", "Can not find a Store for id ");
+    STORE_NOT_FOUND(400, "C003", "Can not find a Store for id "),
+
+    INVALID_INPUT_VALUE(400, "C004", " Invalid Input Value");
 
     private final int status;
     private final String code;
@@ -15,17 +21,5 @@ public enum ErrorCode {
         this.status = status;
         this.code = code;
         this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public int getStatus() {
-        return status;
     }
 }
---
+++ src/main/java/com/coumin/woowahancoupons/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,61 @@
+package com.coumin.woowahancoupons.global.error;
+
+import com.coumin.woowahancoupons.global.ApiResponse;
+import com.coumin.woowahancoupons.global.exception.BusinessException;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	private ResponseEntity<ApiResponse<?>> newResponseEntity(Throwable throwable, HttpStatus status) {
+		Optional<String> message = Arrays.stream(throwable.getStackTrace())
+			.map(StackTraceElement::toString)
+			.reduce((a, b) -> a + " " + b);
+
+		return newResponseEntity(ErrorCode.UNEXPECTED.getCode(),
+			message.orElse(throwable.getMessage()), status);
+	}
+
+	private ResponseEntity<ApiResponse<?>> newResponseEntity(String code, String message, HttpStatus status) {
+		return ResponseEntity.status(status).body(ApiResponse.error(code, message));
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	protected ResponseEntity<ApiResponse<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+		String fieldErrorMessage = e.getBindingResult().getFieldErrors()
+			.stream()
+			.map(error -> String.format("%s %s", error.getField(), error.getDefaultMessage()))
+			.collect(Collectors.joining(","));
+		String errorMessage = String.format("%s: %s", errorCode.getMessage(), fieldErrorMessage);
+		log.error("handleMethodArgumentNotValid exception occurred: {}", errorMessage, e);
+
+		return newResponseEntity(
+			errorCode.getCode(),
+			errorMessage,
+			HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(BusinessException.class)
+	public ResponseEntity<ApiResponse<?>> handleBusinessException(BusinessException e) {
+		log.error("businessException exception occurred: {}", e.getMessage(), e);
+
+		return newResponseEntity(e.getErrorCode().getCode(), e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler({Exception.class, RuntimeException.class})
+	public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
+		log.error("Unexpected exception occurred: {}", e.getMessage(), e);
+
+		return newResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
