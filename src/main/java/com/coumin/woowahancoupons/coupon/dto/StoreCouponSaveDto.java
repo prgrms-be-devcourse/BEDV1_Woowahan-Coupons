437b9afe4025c0b97fb827fa87f9431c2c6b1df4
LeeJuOh <>
2021-10-29 20:55:32 +0900

    ⭐ feat : coupon rest controller 추가

--- src/main/java/com/coumin/woowahancoupons/coupon/dto/StoreCouponSaveDto.java
+++ src/main/java/com/coumin/woowahancoupons/coupon/dto/StoreCouponSaveDto.java
@@ -4,46 +4,57 @@ package com.coumin.woowahancoupons.coupon.dto;
 import com.coumin.woowahancoupons.domain.coupon.Coupon;
 import com.coumin.woowahancoupons.domain.coupon.DiscountType;
 import com.coumin.woowahancoupons.domain.coupon.ExpirationPolicy;
-import com.coumin.woowahancoupons.domain.coupon.ExpirationPolicyType;
 import com.coumin.woowahancoupons.domain.coupon.IssuerType;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import lombok.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StoreCouponSaveDto {
 
-    private String name;
+	@NotEmpty
+	@Size(min = 2, max = 100)
+	private String name;
 
-    private Long amount;
+	@NotNull
+	@Positive
+	private Long amount;
 
-    private Integer daysAfterIssuance;
+	@NotNull
+	@Positive
+	private Integer daysAfterIssuance;
 
-    private Long minOrderPrice;
+	@NotNull
+	@Positive
+	private Long minOrderPrice;
 
-    @Builder
-    public StoreCouponSaveDto(
-        String name,
-        Long amount,
-        Integer daysAfterIssuance,
-        Long minOrderPrice) {
-        this.name = name;
-        this.amount = amount;
-        this.daysAfterIssuance = daysAfterIssuance;
-        this.minOrderPrice = minOrderPrice;
-    }
+	@Builder
+	public StoreCouponSaveDto(
+		String name,
+		Long amount,
+		Integer daysAfterIssuance,
+		Long minOrderPrice) {
+		this.name = name;
+		this.amount = amount;
+		this.daysAfterIssuance = daysAfterIssuance;
+		this.minOrderPrice = minOrderPrice;
+	}
 
-    public Coupon toEntity(Long issuerId) {
-        ExpirationPolicy expirationPolicy = ExpirationPolicy.newByAfterIssueDate(daysAfterIssuance);
+	public Coupon toEntity(Long issuerId) {
+		ExpirationPolicy expirationPolicy = ExpirationPolicy.newByAfterIssueDate(daysAfterIssuance);
 
-        return Coupon.builder(
-            name,
-            amount,
-            expirationPolicy,
-            DiscountType.FIXED_AMOUNT,
-            IssuerType.STORE,
-            issuerId)
-            .minOrderPrice(minOrderPrice)
-            .maxCountPerCustomer(1)
-            .build();
-    }
+		return Coupon.builder(
+			name,
+			amount,
+			expirationPolicy,
+			DiscountType.FIXED_AMOUNT,
+			IssuerType.STORE,
+			issuerId)
+			.minOrderPrice(minOrderPrice)
+			.maxCountPerCustomer(1)
+			.build();
+	}
 }
--- src/main/java/com/coumin/woowahancoupons/coupon/dto/StoreCouponSaveRequestDto.java
+++ src/main/java/com/coumin/woowahancoupons/coupon/dto/StoreCouponSaveRequestDto.java
@@ -1,17 +1,21 @@
 package com.coumin.woowahancoupons.coupon.dto;
 
 import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StoreCouponSaveRequestDto {
 
-    private final List<StoreCouponSaveDto> storeCouponSaveDtos;
+	@Valid
+	@Size(max = 3)
+	private List<StoreCouponSaveDto> storeCouponSaveDtos;
 
-    public StoreCouponSaveRequestDto(
-        List<StoreCouponSaveDto> storeCouponSaveDtos) {
-        this.storeCouponSaveDtos = storeCouponSaveDtos;
-    }
-
-    public List<StoreCouponSaveDto> getStoreCouponSaveDtos() {
-        return storeCouponSaveDtos;
-    }
+	public StoreCouponSaveRequestDto(List<StoreCouponSaveDto> storeCouponSaveDtos) {
+		this.storeCouponSaveDtos = storeCouponSaveDtos;
+	}
 }
