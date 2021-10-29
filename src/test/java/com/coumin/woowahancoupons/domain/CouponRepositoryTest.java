aa98ba3bf64eb99778f7c765b7c21cbc6634e1d6
LeeJuOh <56071126+LeeJuOh@users.noreply.github.com>
2021-10-29 15:27:18 +0900

    [WOOCOU-34] 🚦 test : 쿠폰 repository, service 단위 테스트 추가 (#18)
    * [WOOCOU-34] 🚦 test : 쿠폰 repository, service 단위 테스트 추가
    
    * 🚦 test : 매장 쿠폰 실패 테스트 리팩터링
    
    * 🚦 test : 필요없는 test yml 삭제
    
    * 🚦 test : 변수 네이밍 수정
    
    * 🚦 test : ExpirationPolicy 생성 수정
    
    * 🚦 test :  storeRepository.existsById() 호출 시 return false 하도록 수정
    
    Co-authored-by: LeeJuOh <>

---
+++ src/test/java/com/coumin/woowahancoupons/domain/CouponRepositoryTest.java
@@ -0,0 +1,107 @@
+package com.coumin.woowahancoupons.domain;
+
+import com.coumin.woowahancoupons.domain.coupon.Coupon;
+import com.coumin.woowahancoupons.domain.coupon.CouponRepository;
+import com.coumin.woowahancoupons.domain.coupon.DiscountType;
+import com.coumin.woowahancoupons.domain.coupon.ExpirationPolicy;
+import com.coumin.woowahancoupons.domain.coupon.ExpirationPolicyType;
+import com.coumin.woowahancoupons.domain.coupon.IssuerType;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CouponRepositoryTest {
+
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Autowired
+	private CouponRepository couponRepository;
+
+	@Test
+	@DisplayName("BaseEntity 생성 테스트")
+	void baseEntityAuditingTest() {
+		//Given
+		LocalDateTime now = LocalDateTime.now();
+		Coupon coupon = createCoupons(IssuerType.ADMIN, 1L, 1).get(0);
+		entityManager.persist(coupon);
+		entityManager.clear();
+
+		//When
+		Coupon foundCoupon = couponRepository.findById(coupon.getId()).get();
+
+		//Then
+		SoftAssertions.assertSoftly(softAssertions -> {
+				softAssertions.assertThat(foundCoupon.getCreatedAt()).isAfter(now);
+				softAssertions.assertThat(foundCoupon.getLastModifiedAt()).isAfter(now);
+			}
+		);
+	}
+
+	@Test
+	@DisplayName("쿠폰 생성 테스트")
+	void saveAllCouponTest() {
+		//Given
+		Coupon coupon = createCoupons(IssuerType.ADMIN, 1L, 1).get(0);
+
+		//When
+		couponRepository.save(coupon);
+
+		//Then
+		List<Coupon> allCoupons = couponRepository.findAll();
+
+		SoftAssertions.assertSoftly(softAssertions -> {
+				softAssertions.assertThat(allCoupons).isNotEmpty();
+				softAssertions.assertThat(allCoupons.get(0).getId()).isEqualTo(coupon.getId());
+			}
+		);
+	}
+
+	@Test
+	@DisplayName("발행인의 아이디로 쿠폰 조회 테스트")
+	void findCouponByIssuerIdTest() {
+		//Given
+		long issuerId = 1L;
+		List<Coupon> coupons1 = createCoupons(IssuerType.ADMIN, issuerId, 3);
+		List<Coupon> coupons2 = createCoupons(IssuerType.STORE, 2L, 3);
+
+		coupons1.forEach(coupon -> entityManager.persist(coupon));
+		coupons2.forEach(coupon -> entityManager.persist(coupon));
+		entityManager.clear();
+
+		//When
+		List<Coupon> allCouponsCreatedByAdmin = couponRepository.findByIssuerId(issuerId);
+
+		//Then
+		SoftAssertions.assertSoftly(softAssertions -> {
+				softAssertions.assertThat(allCouponsCreatedByAdmin).hasSize(3);
+			}
+		);
+	}
+
+	private List<Coupon> createCoupons(IssuerType issuerType, long issuerId, int size) {
+
+		ExpirationPolicy expirationPolicy = ExpirationPolicy.newByAfterIssueDate(14);
+
+		return IntStream.range(0, size).mapToObj(i ->
+			Coupon.builder(
+				"testName#" + i,
+				1000L * (i + 1),
+				expirationPolicy,
+				DiscountType.FIXED_AMOUNT,
+				issuerType,
+				issuerId
+			).build()
+		).collect(Collectors.toList());
+	}
+}
