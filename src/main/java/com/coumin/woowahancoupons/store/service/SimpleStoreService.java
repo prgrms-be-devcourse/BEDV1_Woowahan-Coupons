46884e828b3db667c8b9f41ae8704a1b3c04e69e
LeeJuOh <56071126+LeeJuOh@users.noreply.github.com>
2021-10-30 16:00:21 +0900

    [WOOCOU-44] ⭐ feat : 매장의 쿠폰 리스트 조회 서비스 레이어 구현 (#24)
    Co-authored-by: LeeJuOh <>

--- src/main/java/com/coumin/woowahancoupons/store/service/SimpleStoreService.java
+++ src/main/java/com/coumin/woowahancoupons/store/service/SimpleStoreService.java
@@ -1,8 +1,27 @@
 package com.coumin.woowahancoupons.store.service;
 
+import com.coumin.woowahancoupons.domain.coupon.CouponRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
+import com.coumin.woowahancoupons.coupon.dto.StoreCouponResponseDto;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
 @Service
 public class SimpleStoreService implements StoreService {
 
+	private final CouponRepository couponRepository;
+
+	public SimpleStoreService(
+		CouponRepository couponRepository) {
+		this.couponRepository = couponRepository;
+	}
+
+	@Override
+	public List<StoreCouponResponseDto> findStoreCoupons(Long storeId) {
+		return couponRepository.findByIssuerId(storeId).stream()
+			.map(StoreCouponResponseDto::new)
+			.collect(Collectors.toList());
+	}
 }
--- src/main/java/com/coumin/woowahancoupons/store/service/StoreService.java
+++ src/main/java/com/coumin/woowahancoupons/store/service/StoreService.java
@@ -1,5 +1,9 @@
 package com.coumin.woowahancoupons.store.service;
 
+import java.util.List;
+import com.coumin.woowahancoupons.coupon.dto.StoreCouponResponseDto;
+
 public interface StoreService {
 
+	List<StoreCouponResponseDto> findStoreCoupons(Long storeId);
 }
