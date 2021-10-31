b2cdae226414e1078b34cac9f47ad0f39ad44779
maenguin <49011919+maenguin@users.noreply.github.com>
2021-11-01 00:04:53 +0900

    [WOOCOU-59] 🚦 test : 이미 등록한 쿠폰코드 재등록에 대한 테스트 추가 (#33)

--- src/test/java/com/coumin/woowahancoupons/coupon/service/SimpleCouponRedemptionServiceTest.java
+++ src/test/java/com/coumin/woowahancoupons/coupon/service/SimpleCouponRedemptionServiceTest.java
@@ -12,6 +12,7 @@ import com.coumin.woowahancoupons.domain.coupon.CouponRedemptionRepository;
 import com.coumin.woowahancoupons.domain.customer.Customer;
 import com.coumin.woowahancoupons.domain.customer.CustomerRepository;
 import com.coumin.woowahancoupons.global.error.ErrorCode;
+import com.coumin.woowahancoupons.global.exception.CouponRedemptionAlreadyAllocateCustomer;
 import com.coumin.woowahancoupons.global.exception.CouponRedemptionNotFoundException;
 import java.util.Optional;
 import java.util.UUID;
@@ -35,8 +36,8 @@ class SimpleCouponRedemptionServiceTest {
     private SimpleCouponRedemptionService couponRedemptionService;
 
     @Test
-    @DisplayName("고객이 쿠폰 코드를 입력해 쿠폰 발급 - 성공 테스트")
-    void allocateCouponToCustomerSuccessTest() {
+    @DisplayName("고객이 쿠폰 코드를 입력해 쿠폰 등록 - 성공 테스트")
+    void allocateExistingCouponToCustomerSuccessTest() {
         //Given
         Long customerId = 1L;
         Customer mockCustomer = mock(Customer.class);
@@ -61,8 +62,8 @@ class SimpleCouponRedemptionServiceTest {
     }
 
     @Test
-    @DisplayName("고객이 쿠폰 코드를 입력해 쿠폰 발급 - 실패 테스트 (잘못된 쿠폰 Id)")
-    void allocateCouponToCustomerFailureTest() {
+    @DisplayName("고객이 쿠폰 코드를 입력해 쿠폰 등록 - 실패 테스트 (잘못된 쿠폰 Id)")
+    void allocateExistingCouponToCustomerFailureTest() {
         //Given
         UUID invalidId = UUID.randomUUID();
         given(couponRedemptionRepository.findByCouponCode(invalidId)).willReturn(Optional.empty());
@@ -72,5 +73,31 @@ class SimpleCouponRedemptionServiceTest {
             () -> couponRedemptionService.allocateExistingCouponToCustomer(invalidId, 1L))
             .isInstanceOf(CouponRedemptionNotFoundException.class)
             .hasMessageContaining(ErrorCode.COUPON_REDEMPTION_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    @DisplayName("고객이 쿠폰 코드를 입력해 쿠폰 등록 - 실패 테스트 (이미 등록된 쿠폰)")
+    void allocateExistingCouponToCustomerFailureTest2() {
+        //Given
+        Long customerId = 1L;
+        Customer mockCustomer = mock(Customer.class);
+        Coupon coupon = TestCouponFactory.builder().build();
+        CouponRedemption couponRedemption = CouponRedemption.of(coupon);
+
+        given(couponRedemptionRepository.findByCouponCode(couponRedemption.getCouponCode()))
+            .willReturn(Optional.of(couponRedemption));
+        given(customerRepository.getById(customerId)).willReturn(mockCustomer);
+
+        couponRedemptionService.allocateExistingCouponToCustomer(
+            couponRedemption.getCouponCode(),
+            customerId
+        );
+
+        //When Then
+        assertThatThrownBy(() -> couponRedemptionService.allocateExistingCouponToCustomer(
+            couponRedemption.getCouponCode(),
+            customerId))
+            .isInstanceOf(CouponRedemptionAlreadyAllocateCustomer.class)
+            .hasMessageContaining(ErrorCode.COUPON_REDEMPTION_ALREADY_ALLOCATE.getMessage());
     }
 }
