152031ab77d8c1105f96e2e68251ad9bb309f96e
maenguin <maenguin924@gmail.com>
2021-11-01 00:10:29 +0900

    🚦 test : 고객에게 쿠폰 자동 할당 기능 성공 테스트 추가

--- src/test/java/com/coumin/woowahancoupons/coupon/service/SimpleCouponRedemptionServiceTest.java
+++ src/test/java/com/coumin/woowahancoupons/coupon/service/SimpleCouponRedemptionServiceTest.java
@@ -3,12 +3,15 @@ package com.coumin.woowahancoupons.coupon.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 
 import com.coumin.woowahancoupons.coupon.TestCouponFactory;
 import com.coumin.woowahancoupons.domain.coupon.Coupon;
 import com.coumin.woowahancoupons.domain.coupon.CouponRedemption;
 import com.coumin.woowahancoupons.domain.coupon.CouponRedemptionRepository;
+import com.coumin.woowahancoupons.domain.coupon.CouponRepository;
 import com.coumin.woowahancoupons.domain.customer.Customer;
 import com.coumin.woowahancoupons.domain.customer.CustomerRepository;
 import com.coumin.woowahancoupons.global.error.ErrorCode;
@@ -19,12 +22,16 @@ import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class SimpleCouponRedemptionServiceTest {
+
+    @Mock
+    private CouponRepository couponRepository;
 
     @Mock
     private CouponRedemptionRepository couponRedemptionRepository;
@@ -99,5 +106,30 @@ class SimpleCouponRedemptionServiceTest {
             customerId))
             .isInstanceOf(CouponRedemptionAlreadyAllocateCustomer.class)
             .hasMessageContaining(ErrorCode.COUPON_REDEMPTION_ALREADY_ALLOCATE.getMessage());
+    }
+
+    @Test
+    @DisplayName("고객에게 쿠폰 자동 할당 - 성공 테스트")
+    void allocateCouponToCustomerWithIssuanceSuccessTest() {
+        //Given
+        Long customerId = 1L;
+        Long couponId = 1L;
+        Customer mockCustomer = mock(Customer.class);
+        Coupon spyCoupon = spy(TestCouponFactory.builder().build());
+
+        given(spyCoupon.getId()).willReturn(couponId);
+        given(mockCustomer.getId()).willReturn(customerId);
+        given(couponRepository.findById(couponId)).willReturn(Optional.of(spyCoupon));
+        given(customerRepository.findById(customerId)).willReturn(Optional.of(mockCustomer));
+
+        //When
+        couponRedemptionService.allocateCouponToCustomerWithIssuance(spyCoupon.getId(), customerId);
+
+        //Then
+        ArgumentCaptor<CouponRedemption> captor = ArgumentCaptor.forClass(CouponRedemption.class);
+        then(couponRedemptionRepository).should().save(captor.capture());
+        CouponRedemption captorCouponRedemption = captor.getValue();
+        assertThat(captorCouponRedemption.getCoupon().getId()).isEqualTo(couponId);
+        assertThat(captorCouponRedemption.getCustomer().getId()).isEqualTo(customerId);
     }
 }
