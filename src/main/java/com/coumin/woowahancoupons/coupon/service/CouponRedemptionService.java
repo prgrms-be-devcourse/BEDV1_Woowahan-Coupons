00ddf734f80c9794548eb1f17a6a64244fcabcb2
maenguin <maenguin924@gmail.com>
2021-10-31 12:57:12 +0900

    ♻️ refactor : allocateCouponToCustomer -> allocateExistingCouponToCustomer으로 이름 변경

--- src/main/java/com/coumin/woowahancoupons/coupon/service/CouponRedemptionService.java
+++ src/main/java/com/coumin/woowahancoupons/coupon/service/CouponRedemptionService.java
@@ -3,5 +3,5 @@ package com.coumin.woowahancoupons.coupon.service;
 import java.util.UUID;
 
 public interface CouponRedemptionService {
-    void allocateCouponToCustomer(UUID couponId, Long customerId);
+    void allocateExistingCouponToCustomer(UUID couponCode, Long customerId);
 }
