853454b93b6103148ae750d1f20c6b0661172ed2
LeeJuOh <56071126+LeeJuOh@users.noreply.github.com>
2021-11-01 14:30:18 +0900

    [WOOCOU-46] ⭐ feat : 매장 쿠폰 리스트 조회 컨트롤러 구현 (#37)
    * ⭐ feat : 매장 쿠폰 리스트 조회 컨트롤러 구현
    
    * ⚙️ chore : 개행 삭제
    
    * 🚦 test : response body 검사 추가
    
    Co-authored-by: LeeJuOh <>

--- src/main/java/com/coumin/woowahancoupons/store/controller/StoreRestController.java
+++ src/main/java/com/coumin/woowahancoupons/store/controller/StoreRestController.java
@@ -1,8 +1,27 @@
 package com.coumin.woowahancoupons.store.controller;
 
+import com.coumin.woowahancoupons.coupon.dto.StoreCouponResponseDto;
+import com.coumin.woowahancoupons.global.ApiResponse;
+import com.coumin.woowahancoupons.store.service.StoreService;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/v1/stores")
 @RestController
 public class StoreRestController {
 
+    private final StoreService storeService;
+
+    public StoreRestController(StoreService storeService) {
+        this.storeService = storeService;
+    }
+
+    @GetMapping("/{storeId}/coupons")
+    public ApiResponse<List<StoreCouponResponseDto>> getStoreCoupons(@PathVariable long storeId) {
+        List<StoreCouponResponseDto> responseDtoList = storeService.findStoreCoupons(storeId);
+        return ApiResponse.success(responseDtoList);
+    }
 }
---
+++ src/test/java/com/coumin/woowahancoupons/store/controller/StoreRestControllerTest.java
@@ -0,0 +1,89 @@
+package com.coumin.woowahancoupons.store.controller;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.coumin.woowahancoupons.coupon.TestCouponFactory;
+import com.coumin.woowahancoupons.domain.coupon.Coupon;
+import com.coumin.woowahancoupons.domain.coupon.CouponRepository;
+import com.coumin.woowahancoupons.domain.coupon.DiscountType;
+import com.coumin.woowahancoupons.domain.coupon.ExpirationPolicy;
+import com.coumin.woowahancoupons.domain.coupon.IssuerType;
+import com.coumin.woowahancoupons.domain.store.Store;
+import com.coumin.woowahancoupons.domain.store.StoreRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+class StoreRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Autowired
+    private StoreRepository storeRepository;
+
+    @Test
+    @DisplayName("매장의 쿠폰 리스트 조회 요청 성공")
+    void findStoreCouponsTest() throws Exception {
+        //Given
+        long storeId = storeRepository.save(new Store("testStore#1")).getId();
+        List<Coupon> storeCoupons = LongStream.range(0, 3)
+            .mapToObj(i -> TestCouponFactory.builder()
+                .name("store coupon#" + i)
+                .amount(1000 * (i + 1))
+                .expirationPolicy(ExpirationPolicy.newByAfterIssueDate(14))
+                .discountType(DiscountType.FIXED_AMOUNT)
+                .minOrderPrice(1000 * (i + 1))
+                .issuerType(IssuerType.STORE)
+                .issuerId(storeId)
+                .build())
+            .collect(Collectors.toList());
+        couponRepository.saveAll(storeCoupons);
+
+        //When
+        ResultActions resultActions = requestGetStoreCoupons(storeId);
+
+        //Then
+        resultActions
+            .andExpect(status().isOk())
+            .andExpect(handler().handlerType(StoreRestController.class))
+            .andExpect(handler().methodName("getStoreCoupons"))
+            .andExpect(jsonPath("$.success", is(true)))
+            .andExpect(jsonPath("$.data", hasSize(storeCoupons.size())))
+            .andExpect(jsonPath("$.data[0].id", is(storeCoupons.get(0).getId().intValue())))
+            .andExpect(jsonPath("$.data[0].name", is(storeCoupons.get(0).getName())))
+            .andExpect(jsonPath("$.data[0].amount", is(storeCoupons.get(0).getAmount().intValue())))
+            .andExpect(jsonPath("$.data[0].minOrderPrice", is(storeCoupons.get(0).getMinOrderPrice().intValue())))
+            .andExpect(jsonPath("$.data[0].daysAfterIssuance", is(storeCoupons.get(0).getExpirationPolicy().getDaysFromIssuance())))
+            .andExpect(jsonPath("$.error", is(nullValue())));
+    }
+
+    private ResultActions requestGetStoreCoupons(long storeId) throws Exception {
+        return mockMvc.perform(get("/api/v1/stores/{storeId}/coupons", storeId)
+            .accept(MediaType.APPLICATION_JSON)
+        ).andDo(print());
+    }
+}
