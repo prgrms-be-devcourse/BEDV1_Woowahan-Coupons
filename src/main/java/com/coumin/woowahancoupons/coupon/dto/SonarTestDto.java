a24dbab327eeb3d321ba99134c23e864d2811f01
LeeJuOh <>
2021-11-01 21:16:24 +0900

    Add sonar test class

---
+++ src/main/java/com/coumin/woowahancoupons/coupon/dto/SonarTestDto.java
@@ -0,0 +1,16 @@
+package com.coumin.woowahancoupons.coupon.dto;
+
+import java.util.stream.IntStream;
+
+public class SonarTestDto {
+
+    String snake_case;
+
+    public SonarTestDto(String snake_case) {
+        this.snake_case = snake_case;
+    }
+
+    public String getSnake_case() {
+        return snake_case;
+    }
+}
