c028a776b489380fe360bf2d0e255b10ea495d78
maenguin <maenguin924@gmail.com>
2021-11-04 11:36:55 +0900

    ♻️ refactor : countByCouponIdAndCustomerId 쿼리 이름 기능 -> 직접 쿼리 작성으로 변경

--- src/main/java/com/coumin/woowahancoupons/domain/coupon/CouponRedemptionRepository.java
+++ src/main/java/com/coumin/woowahancoupons/domain/coupon/CouponRedemptionRepository.java
@@ -6,6 +6,8 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CouponRedemptionRepository extends JpaRepository<CouponRedemption, Long> {
 
@@ -14,5 +16,7 @@ public interface CouponRedemptionRepository extends JpaRepository<CouponRedempti
     @EntityGraph(attributePaths = {"coupon"}, type = EntityGraphType.LOAD)
     List<CouponRedemption> findByCustomerIdAndUsedFalse(Long customerId);
 
-    int countByCouponIdAndCustomerId(Long couponId, Long customerId);
+    @Query("select count(cr) from CouponRedemption cr"
+        + " where cr.coupon.id = :couponId and cr.customer.id = :customerId")
+    int countByCouponIdAndCustomerId(@Param("couponId") Long couponId, @Param("customerId") Long customerId);
 }
