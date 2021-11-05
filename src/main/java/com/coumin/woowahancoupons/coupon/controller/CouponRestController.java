b99a907d16aa0c0ad670e6111167da71562ec6ab
LeeJuOh <>
2021-11-05 17:01:35 +0900

    ♻️ refactor : @validate 삭제

--- src/main/java/com/coumin/woowahancoupons/coupon/controller/CouponRestController.java
+++ src/main/java/com/coumin/woowahancoupons/coupon/controller/CouponRestController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Validated
 @RestController
 @RequestMapping("api/v1/coupons")
 public class CouponRestController {
